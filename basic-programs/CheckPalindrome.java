21cc88436c0ac7f641626ce06c0bc6f3da450618
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-10 15:30:09 +0530

    feat:added solution for checking palindrome

---
+++ basic-programs/CheckPalindrome.java
@@ -0,0 +1,21 @@
+// 08. To find out whether the given String is Palindrome or not.
+
+import java.util.Scanner;
+
+public class CheckPalindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter string: ");
+        String inputString = input.nextLine();
+        int length = inputString.length();
+        String reverseString = "";
+        for (int i = length - 1; i >= 0; i--) {
+            reverseString = reverseString + inputString.charAt(i);
+        }
+        if (reverseString.equals(inputString)) {
+            System.out.println("Yes it is a Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
