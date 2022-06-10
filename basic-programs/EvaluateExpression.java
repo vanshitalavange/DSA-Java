9b696e892ab5fb0fd1fcf29b865223ddeb417d2f
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-10 13:39:02 +0530

    feat: added solution for evaluate expression problem

---
+++ basic-programs/EvaluateExpression.java
@@ -0,0 +1,26 @@
+
+// 04. Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+import java.util.Scanner;
+
+public class EvaluateExpression {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter operand 1: ");
+        float operand1 = input.nextFloat();
+        System.out.print("Enter operand 2: ");
+        float operand2 = input.nextFloat();
+        System.out.print("Enter operator : ");
+        String operator = input.next();
+        float result = 0;
+        if (operator.equals("+")) {
+            result = operand1 + operand2;
+        } else if (operator.equals("-")) {
+            result = operand1 - operand2;
+        } else if (operator.equals("*")) {
+            result = operand1 * operand2;
+        } else if (operator.equals("/")) {
+            result = operand1 / operand2;
+        }
+        System.out.println("Result = " + result);
+    }
+}
