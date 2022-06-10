6f7613613fb2e7cb1d2dedfaea8c1ce0fe830ec6
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-10 14:06:09 +0530

    feat:added solution for rupees to USD

---
+++ basic-programs/CurrencyToUSD.java
@@ -0,0 +1,13 @@
+// 06. Input currency in rupees and output in USD.
+
+import java.util.Scanner;
+
+public class CurrencyToUSD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter currency in rupees: ");
+        float rupees = input.nextFloat();
+        float USD = rupees * 77.82f;
+        System.out.println("Currency in USD = " + USD);
+    }
+}
