c591c51fd87e13b7bbd3f07d761b8d85b2ef0448
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-10 16:53:54 +0530

    feat: added solution to check case

---
+++ basic-programs/CheckCase.java
@@ -0,0 +1,16 @@
+
+// 10. Check case of the entered string 
+import java.util.Scanner;
+
+public class CheckCase {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        char firstLetter = input.next().trim().charAt(0);
+        if (firstLetter >= 'a' && firstLetter <= 'z') {
+            System.out.println("Lowercase");
+        } else {
+            System.out.println("Uppercase");
+        }
+    }
+}
