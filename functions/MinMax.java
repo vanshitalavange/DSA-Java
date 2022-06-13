85cab0ab5d976abb2c319544e87fb3b9571278bf
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-13 09:25:45 +0530

    feat: added solution for  min max

---
+++ functions/MinMax.java
@@ -0,0 +1,29 @@
+package functions;
+
+// Q. Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int min = getMin(a, b, c);
+        int max = getMax(a, b, c);
+        System.out.println("Min = " + min + " Max = " + max);
+    }
+
+    static int getMin(int a, int b, int c) {
+        int min = a > b ? b : a;
+        min = min > c ? c : min;
+        return min;
+    }
+
+    static int getMax(int a, int b, int c) {
+        int max = a > b ? a : b;
+        max = max > c ? max : c;
+        return max;
+    }
+}
