f5d3676fbc06d69b231aed84c4b26b79cf4f6439
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-14 21:28:50 +0530

    question added

--- arrays/RunningSum1D.java
+++ arrays/RunningSum1D.java
@@ -1,3 +1,9 @@
+/*
+Q. Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+ */
 package arrays;
 
 import java.util.Arrays;
