fc253a9e1928afdf9a686c4fdd08edd11a528fff
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-15 17:00:06 +0530

    feat: solution for number of good pairs

---
+++ arrays/NumberOfGoodPairs.java
@@ -0,0 +1,22 @@
+package arrays;
+import java.util.Arrays;
+
+public class NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+        int count = 0;
+        System.out.println("Input: "+Arrays.toString(nums));
+        System.out.println("Output: "+numIdenticalPairs(nums));
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j])
+                    count++;
+            }
+        }
+        return count;
+    }
+}
