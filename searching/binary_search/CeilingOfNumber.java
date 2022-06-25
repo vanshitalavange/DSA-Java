6fd853ca514849dfd1fd15e48f52f7f0c5639083
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-25 15:14:20 +0530

    feat: solution for ceiling of matrix

---
+++ searching/binary_search/CeilingOfNumber.java
@@ -0,0 +1,36 @@
+// Q. Find the smallest element in the array >= target 
+package searching.binary_search;
+
+public class CeilingOfNumber {
+    public static void main(String[] args) {
+        int[] arr = { 18, 15, 12, 10, 9, 7, 5, 3 };
+        int target = 6;
+        System.out.println("Output: " + findCeiling(arr, target));
+    }
+
+    static int findCeiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isAscending = arr[start] < arr[end];
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (isAscending) {
+                if (target > arr[mid]) {
+                    start = mid + 1;
+                } else if (target < arr[mid]) {
+                    end = mid - 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else if (target < arr[mid]) {
+                    start = mid + 1;
+                }
+            }
+        }
+        return isAscending ? arr[start] : arr[end];
+    }
+
+}
