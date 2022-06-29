887b7727b47b5ae8e6477c8785ac36762f433322
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-06-29 17:08:30 +0530

    feat: solution for perfect square problem

---
+++ searching/binary_search/IsPerfectSquare.java
@@ -0,0 +1,24 @@
+// Question link: https://leetcode.com/problems/valid-perfect-square/
+package searching.binary_search;
+
+public class IsPerfectSquare {
+    public static void main(String[] args) {
+        int num = 16;
+        System.out.println("Output: "+isPerfectSquare(num));
+    }
+    public static boolean isPerfectSquare(int num) {
+        int start = 0;
+        int end = num < 46340 ? num/2 : 46340;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(mid*mid == num){
+                return true;
+            }else if(mid*mid > num){
+                end = mid -1 ;
+            }else if(mid*mid <num){
+                start = mid+1;
+            }
+        }
+        return num == 1 ? true : false;
+    }
+}
