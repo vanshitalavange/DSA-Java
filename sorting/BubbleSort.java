d4729b62824037d4836aaba95b208a7601136f6d
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-07-23 20:20:08 +0530

    feat: bubble sort algorithm

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package sorting;
+import java.util.*;
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,2,3,3,1};
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+
+    static int[] bubbleSort(int[] arr){
+        boolean isSwapped;
+        for(int i=0;i<arr.length-1;i++){
+            isSwapped = false;
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped){
+                return arr;
+            }
+        }
+        return arr;
+    }
+}
