8171d7638219ed4ac840c9e44867d1dc657e6155
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-07-24 11:18:01 +0530

    feat: selection sort algorithm

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package sorting;
+
+import java.util.*;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 1,2,-5,-6,8,9 };
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
+
+    static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxOnIndex = findMax(arr,0,last);
+            int temp = arr[last];
+            arr[last] = arr[maxOnIndex];
+            arr[maxOnIndex] = temp;
+        }
+        return arr;
+    }
+
+    static int findMax(int[] arr, int start, int end) {
+        int maxOnIndex = start;
+        for (int i = 0; i <=end; i++) {
+            if (arr[i] > arr[maxOnIndex]) {
+                maxOnIndex = i;
+            }
+        }
+        return maxOnIndex;
+    }
+}
