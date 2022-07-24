c32a840611e47081cdc10344b6fb12d497c6164e
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-07-24 14:08:20 +0530

    code format

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {5,3,4,2,1};
+        int[] arr = {5,4,3,2,1};
         System.out.println(Arrays.toString(insertionSort(arr)));
     }
 
@@ -16,7 +16,7 @@ public class InsertionSort {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
-               }else{
+                }else{
                     break;
                 }
             }
