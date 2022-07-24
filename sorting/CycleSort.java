c083f234c4673da83eeb17fb930c98dab64dfac1
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-07-24 14:08:07 +0530

    feat: cycle sort

---
+++ sorting/CycleSort.java
@@ -0,0 +1,26 @@
+// Use cycle sort only if given numbers are from 1 to N
+package sorting;
+
+import java.util.Arrays;
+
+public class CycleSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,2,3,1};
+        System.out.println(Arrays.toString(cycleSort(arr)));
+    }
+
+    static int[] cycleSort(int[] arr){
+        int i=0;
+        while(i < arr.length){
+            int indexOfElement = arr[i] - 1;
+            if(i != indexOfElement){
+                int temp = arr[indexOfElement];
+                arr[indexOfElement] = arr[i];
+                arr[i] = temp; 
+            }else{
+                i++;
+            }
+        }
+        return arr;
+    }
+}
