27db56bff3d47c06f8e28a55e5963be270b56809
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-08-27 12:47:19 +0530

    feat: pattern 6

---
+++ patterns/Pattern_06.java
@@ -0,0 +1,36 @@
+/*
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+*/
+package patterns;
+
+class Pattern_06 {
+    public static void main(String[] args) {
+        pattern6(5);
+    }
+
+    static void pattern6(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            int spaces = n - i;
+            for (int j = 1; j <= n; j++) {
+                if (j <= spaces) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
