5a9c1059420fda80a1cb963ef11607670fcb426a
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-08-27 14:42:05 +0530

    feat: pattern 10

--- patterns/Pattern_10.java
+++ patterns/Pattern_10.java
@@ -17,15 +17,15 @@ class Pattern_10 {
     static void pattern10(int n) {
 
         for (int i = 1; i <= n; i++) {
-            int stars = i ;
+            int stars = i;
             int startPos = n - (i - 1);
             for (int j = 1; j <= n + n - 1; j++) {
                 if (j >= startPos) {
-                    while(stars > 0){
+                    while (stars > 0) {
                         System.out.print("* ");
                         stars--;
                     }
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
