39d81584dc0779ce067703b43acafbc6954b9103
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-09-03 22:42:10 +0530

    feat: pattern 32

---
+++ patterns/Pattern_32.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class Pattern_32 {
+    public static void main(String[] args) {
+        pattern32(5);
+    }
+
+    static void pattern32(int n) {
+        char ch = (char) (64 + n);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                int ascii = (int) ch;
+                ascii++;
+                ch = (char) ascii;
+            }
+            ch = (char) (64 + (n - i));
+            System.out.println();
+        }
+    }
+}
