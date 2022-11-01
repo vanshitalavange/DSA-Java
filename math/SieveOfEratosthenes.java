4da414b41abc2703ce124e835f9e9c8bf366363a
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-11-01 18:08:38 +0530

    math: sieve of eratosthenes optimized

--- math/SieveOfEratosthenes.java
+++ math/SieveOfEratosthenes.java
@@ -1,58 +1,36 @@
 
 package math;
+
 import java.util.*;
+
 public class SieveOfEratosthenes {
     public static void main(String[] args) {
         System.out.println(findPrimeNumbersTillN(40));
     }
+
     public static ArrayList findPrimeNumbersTillN(int n) {
-        int[] arr = new int[n - 1];
-        int sqrt = (int) Math.sqrt(n);
-        int i = 0;
-        while (i <= sqrt) {
-            int indexValue = i + 2;
-            if (arr[i] == 0) {
-                // check if the number is prime
-                if (isPrime(indexValue)) {
-                    arr[i] = 1;
-                    //eliminate all the multiples
-                    eliminateMultiples(indexValue, arr, i + 1);
+        boolean[] arr = new boolean[n + 1];
+        // false -> number is prime
+        // true -> number is not prime
+
+        for (int i = 2; i * i <= n; i++) {
+
+            if (!arr[i]) {
+                // eliminate all the multiples of i
+                for (int j = i * 2; j <= n; j += i) {
+                    arr[j] = true;
                 }
             }
-            i++;
         }
 
         ArrayList<Integer> primeNos = new ArrayList<>();
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[j] == 1 || arr[j] == 0) {
-                primeNos.add(j + 2);
+        for (int i = 2; i <= n; i++) {
+            if (!arr[i]) {
+                primeNos.add(i);
             }
         }
-
         return primeNos;
 
-    }
-
-    public static void eliminateMultiples(int n, int[] arr, int startIndex) {
-
-        for (int i = startIndex; i < arr.length; i++) {
-            int indexValue = i + 2;
-            if (indexValue % n == 0) {
-                arr[i] = -1;
-            }
-        }
-
-    }
-
-    public static boolean isPrime(int n) {
-        int i = 2;
-        while (i * i <= n) {
-            if (n % i == 0) {
-                return false;
-            }
-            i++;
-        }
-        return true;
     }
 
 }
